175b2ae95cb8a690f6ea552550035065243d784f
leticialmb <leticiabarreto196@gmail.com>
2020-04-07 15:12:41 -0400

    Mobile

--- mobile/src/pages/Pets/styles.js
+++ mobile/src/pages/Pets/styles.js
@@ -1,6 +1,10 @@
 import styled from 'styled-components/native';
+import { Platform } from 'react-native';
 
-export const Container = styled.SafeAreaView`
+export const Container = styled.KeyboardAvoidingView.attrs({
+  enabled: Platform.OS === 'ios',
+  behavior: 'padding',
+})`
   flex:1;
 	margin: 50px 5px;
 `;
