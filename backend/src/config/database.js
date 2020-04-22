82b75b68318b4cb43e9b6e80b03c5dc81e0827db
leticialmb <leticiabarreto196@gmail.com>
2020-04-22 09:37:42 -0400

    update

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,21 +2,13 @@ require('dotenv/config');
 
 module.exports = {
 	dialect: 'postgres',
+	host: process.env.DB_HOST,
 	username: process.env.DB_USER,
 	password: process.env.DB_PASS,
 	database: process.env.DB_NAME,
 	define: {
 		timestamps: true,
 		underscored: true,
-		underscoredAll: true,
-	},
-	// username: process.env.DATABASE_USER,
-	// password: process.env.DATABASE_PASS,
-	// database: process.env.DATABASE_NAME,
-	// port:process.env.DATABASE_PORT,
-	// define: {
-	// 	timestamps: true,
-	// 	underscored: true,
-	// 	underscoredAll: true,
-	// },
+		underscoredAll: true
+	}
 };
